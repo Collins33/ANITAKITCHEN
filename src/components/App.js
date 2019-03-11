acdb1c4610e60cf27e19d4dc6cd757210c7757a6
collins njau <collinsnjau39@gmail.com>
2019-03-11 18:32:32 +0300

    Setup basic router for the whole application

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+import Header from './Header';
+import Teachers from './Teachers';
+import Courses from './Courses';
+
 const App = () => (
   // wrap the main component inside browserRouter tags
+  // exact ensures that it only renders the component when the url fully matches
+
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home}/>
+      <Header />
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/courses" component={Courses}/>
+      <Route path="/teachers" component={Teachers}/>
     </div> 
   </BrowserRouter>
   
